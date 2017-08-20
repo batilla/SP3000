42093b35744377ba24b97f5522e0cfb686a85e15
tilla.bakos <tilla.bakos@gmail.com>
2017-08-20 08:20:59 +0200

    added script for tamplate to render data in table

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, redirect, request
+import csv
 
 app = Flask(__name__)
 
-@app.route('/')
-def show_form():
-    return render_template('form.html')
+data_file = "data.csv"
+def read_file(data_file):
+    datafile = open(data_file, 'r')
+    datareader = csv.reader(datafile,delimiter='\n')
+    story_byrow = []
+    r = [] 
+    for row in datareader:
+        for i in row:
+            print(type(i))
+            r = i.split("    ")
+        story_byrow.append(r)    
+    return story_byrow
 
-if __name__ == "__main__":
+@app.route('/') 
+def route_list():
+    return render_template('storyboard.html', story_byrow =read_file(data_file))
+
+if __name__ == '__main__':
     app.run(
-        debug=True,
-        port=5000
+            debug=True,
+            port=5000
     )
